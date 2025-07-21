acde83ed16cfd91de14bde2d18064fe9fe66c8d9
HarshanaPrabhath <harshanaprabhath147@gmail.com>
2025-07-22 02:51:30 +0530

    startning movvement with dependencies

---
+++ ecom-front/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="flex text-8xl font-bold items-center">
+        Hello world!
+      </h1>
+
+    </>
+  );
+}
+
+export default App;
